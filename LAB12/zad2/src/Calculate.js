e64194dbe068c5c5675a36b756e3a57f4f371835
sebciulina <sgransicki@gmail.com>
2023-01-09 17:10:54 +0100

    l12z2 to fix

---
+++ LAB12/zad2/src/Calculate.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export function Add() {
+    const [result, setResult] = useState("");
+    let [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const x = parseInt(searchParams.get("x"));
+        const y = parseInt(searchParams.get("y"));
+
+        setResult(`${x + y}`);
+    }, []);
+
+    return (
+        <div>
+            {result}
+        </div>
+    )
+}
+
+export function Sub() {
+    const [result, setResult] = useState("");
+    let [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const x = parseInt(searchParams.get("x"));
+        const y = parseInt(searchParams.get("y"));
+
+        setResult(`${x - y}`);
+    }, []);
+
+    return (
+        <div>
+            {result}
+        </div>
+    )
+}
+
+export function Mul() {
+    const [result, setResult] = useState("");
+    let [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const x = parseInt(searchParams.get("x"));
+        const y = parseInt(searchParams.get("y"));
+
+        setResult(`${x * y}`);
+    }, []);
+
+    return (
+        <div>
+            {result}
+        </div>
+    )
+}
+
+export function Div() {
+    const [result, setResult] = useState("");
+    let [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const x = parseInt(searchParams.get("x"));
+        const y = parseInt(searchParams.get("y"));
+
+        setResult(`${x / y}`);
+    }, []);
+
+    return (
+        <div>
+            {result}
+        </div>
+    )
+}
